71f79813d8805ea20bd1ec9afbd6c6b4c2f54279
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-05-21 15:10:25 -0400

    fixed some syntax errors

---
+++ output/example.asm
@@ -0,0 +1,80 @@
+@5
+D=A
+@0
+A = M
+M = D
+@0
+M = M + 1
+@3
+D=A
+@0
+A = M
+M = D
+@0
+M = M + 1
+//add
+@0
+M=M-1
+A=M
+D=M
+@5
+M=D
+11
+D=M
+@6
+D=D+A
+@6
+M=D
+@5
+D=M
+@6
+A=M
+M=D
+@0
+M=M-1
+A=M
+D=M
+@5
+M=D
+12
+D=M
+@7
+D=D+A
+@6
+M=D
+@5
+D=M
+@6
+A=M
+M=D
+@11
+D=M
+@12
+D=D+M
+@10
+M=D
+//push temp 5
+@10
+D=M
+@0
+A = M
+M = D
+@0
+M = M + 1
+@0
+M=M-1
+A=M
+D=M
+@5
+M=D
+@1
+D=M
+@0
+D=D+A
+@6
+M=D
+@5
+D=M
+@6
+A=M
+M=D

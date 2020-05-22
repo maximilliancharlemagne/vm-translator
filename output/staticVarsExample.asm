7b5ae5db105b6ce20e923b9b3b8c2ad3e9751511
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-05-22 08:50:31 -0400

    formatting+functionality fixes to eqHandler

--- output/staticVarsExample.asm
+++ output/staticVarsExample.asm
@@ -90,7 +90,49 @@ A = M
 M = D
 @0
 M = M + 1
-//did not add eq instructions?
+//eq
+//pop temp 0
+@0
+M=M-1
+A=M
+D=M
+@5
+M=D
+//pop temp 1
+@0
+M=M-1
+A=M
+D=M
+@6
+M=D
+//eq code
+@5
+D=M
+@6
+D=D-M
+@False
+D;JNE
+(True)
+@0
+D=A
+@1
+D=D-A
+@7
+M=D
+temp
+2
+@End
+0;JMP
+(False)
+//push const 0
+@0
+D=A
+@0
+A = M
+M = D
+@0
+M = M + 1
+(End)
 //pop static 0
 @0
 M=M-1
